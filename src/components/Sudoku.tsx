b12f87d068b3153e2b976a88bd12e9bcd5c02255
Guilherme Coelho <guilhermervcoelho@gmail.com>
2022-01-04 20:52:32 -0300

    sudoku: create grid and table

--- src/components/Sudoku.tsx
+++ src/components/Sudoku.tsx
@@ -1,15 +1,72 @@
 import React from 'react'
 import styled from 'styled-components'
+import * as R from 'ramda'
 
-const Button = styled.button`
-  background: green
+const borderLight = '1px solid rgba(0,0,0,.3)'
+const borderBold = '2px solid red'
+
+const Grid = styled.table`
+  border-spacing: 0;
+  border-collapse: collapse;
+  box-sizing: border-box;
+  border: ${borderLight};
+  border-width: 3px;
+`
+
+const Row = styled.tr`
+  height: 48px;
+  display: flex;
+  box-sizing: border-box;
+  border-collapse: collapse;
+  border-bottom: ${borderLight};
+
+  &:nth-child(3n) {
+    border-width: 3px;
+  }
+
+  &:last-child {
+    border: none;
+  }
+`
+
+const Cell = styled.td`
+  width: 48px;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-collapse: collapse;
+  box-sizing: border-box;
+  border-right: ${borderLight};
+
+  &:nth-child(3n) {
+    border-width: 3px;
+  }
+
+  &:last-child {
+    border: none;
+  }
 `
 
 export default () => {
+  const GRID_SIZE = 9
+
+  const createGrid = R.pipe(
+    () => R.range(0, GRID_SIZE),
+    R.map(
+      (x) => R.range(0, GRID_SIZE)
+    )
+  )
+
+  const grid = createGrid()
+
   return (
-    <>
-      <div>Hey</div>
-      <Button>hello</Button>
-    </>
+    <Grid>
+      {grid.map((row) => (
+        <Row>{row.map((cell) => (
+          <Cell>{cell}</Cell>
+        ))}</Row>
+      ))}
+    </Grid>
   )
 }
